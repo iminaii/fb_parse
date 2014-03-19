21b87f5d1d0678c6b32ab0188aae78b2fcaa0601
Imaan Minaii <imaan@appfly.com>
2014-03-19 13:51:37 +0000

    v0.0.3

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Ruby Gem to parse an ID from various Facebook URLs
 
+Requires getting the URL from a specific place, not really acceptable for end user use but is functional enough to provide in a CMSas you can directly instruct end user.
+
 ## Installation
 
 Add this line to your application's Gemfile:
@@ -18,7 +20,13 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+To return an ID, run FbParse.get_id(url), where url is taken from the address bar on the individual link page.
+
+How to get a correctly formatted URL:
+
+1. Find post on Facebook
+2. Click on the date link on the post
+3. Copy URL from your address bar
 
 ## Contributing
 
--- lib/fb_parse.rb
+++ lib/fb_parse.rb
@@ -1,28 +1,21 @@
 require "fb_parse/version"
 
 module FbParse
-  def self.get_id_status(url)
+  def self.get_id(url)
+    if url.include? 'photo'
+      self.get_id_photo_video(url)
+    else
+      self.get_id_status_link_checkin(url)
+    end
+  end
+
+  def self.get_id_status_link_checkin(url)
     @id = url.split('/')[5].split('?')[0]
     return @id
   end
 
-  def self.get_id_photo(url)
+  def self.get_id_photo_video(url)
     @id = url.split('/')[3].split('=')[1].split('&')[0]
-    return @id
-  end
-
-  def self.get_id_video(url)
-    @id = url.split('/')[3].split('=')[1].split('&')[0]
-    return @id
-  end
-
-  def self.get_id_link(url)
-    @id = url.split('/')[5].split('?')[0]
-    return @id
-  end
-
-  def self.get_id_checkin(url)
-    @id = url.split('/')[5].split('?')[0]
     return @id
   end
 end
