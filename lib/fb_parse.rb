9276bb99fe213c3888e1674eb16d3f8579fae141
Imaan Minaii <imaan@appfly.com>
2014-03-28 11:09:49 +0000

    updates

--- lib/fb_parse.rb
+++ lib/fb_parse.rb
@@ -1,9 +1,24 @@
 require "fb_parse/version"
 
 module FbParse
-  def self.get_id(url)
+  def self.get_id_embed(code)
+    url = URI.extract(code)[0]
+    if url.include? '?fbid='
+      self.get_id_photo_video(url)
+    elsif url.include? 'activity'
+      self.get_id_feed(url)
+    elsif url.include? 'photos'
+      self.get_id_photo_other(url)
+    else
+      self.get_id_status_link_checkin(url)
+    end
+  end
+
+  def self.get_id_url(url)
     if url.include? 'photo'
       self.get_id_photo_video(url)
+    elsif url.include? 'activity'
+      self.get_id_feed
     else
       self.get_id_status_link_checkin(url)
     end
@@ -16,6 +31,16 @@ module FbParse
 
   def self.get_id_photo_video(url)
     @id = url.split('/')[3].split('=')[1].split('&')[0]
+    return @id
+  end
+
+  def self.get_id_feed(url)
+    @id = url.split('/').last.split('?').first
+    return @id
+  end
+
+  def self.get_id_photo_other(url)
+    @id = url.split('/')[6]
     return @id
   end
 end
